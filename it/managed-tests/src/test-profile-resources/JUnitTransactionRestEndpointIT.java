65607fa1a243556c9a24381281af693229250f89
Charles Tian <ctian@us.ibm.com>
2018-01-11 15:07:36 -0600

    Added managed-tests folder correctly

---
+++ it/managed-tests/src/test-profile-resources/JUnitTransactionRestEndpointIT.java
@@ -0,0 +1,78 @@
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import org.jboss.arquillian.container.test.api.Deployment;
+import org.jboss.arquillian.junit.Arquillian;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.WebArchive;
+
+@RunWith(Arquillian.class)
+public class JUnitTransactionRestEndpointIT {
+
+    @Deployment
+    public static WebArchive createDeployment() {
+        return ShrinkWrap.create(WebArchive.class).addPackage("application.rest").addPackage("application");
+    }
+
+    @Test
+    public void testRunningOnServer() throws Exception {
+        System.out.println("--- test running on server");
+        Properties p = System.getProperties();
+        Enumeration<Object> keys = p.keys();
+        while (keys.hasMoreElements()) {
+            String key = (String) keys.nextElement();
+            String value = (String) p.get(key);
+            if (key.equals("wlp.process.type") && value.equals("server")) {
+                return;
+            }
+        }
+        assertTrue(false);
+    }
+
+    @Test
+    public void testDataExistsAtEndpoint() throws Exception {
+        System.out.println("--- test data exists at endpoint");
+        URL endpoint = new URL("http://localhost:9080/myLibertyApp/api/transactions");
+        System.out.println("endpoint url: " + endpoint);
+        String body = readAllAndClose(endpoint.openStream());
+        int bodyLength = body.length();
+        System.out.println("body length is " + bodyLength);
+        assertTrue(bodyLength > 0);
+    }
+
+    @Test
+    public void testNewDataAddedToEndpoint() throws Exception {
+        System.out.println("--- test new data added to endpoint");
+        String firstBody = readAllAndClose(new URL("http://localhost:9080/myLibertyApp/api/transactions").openStream());
+        int firstBodyLength = firstBody.length();
+        Thread.sleep(12000);
+        String secondBody = readAllAndClose(
+                new URL("http://localhost:9080/myLibertyApp/api/transactions").openStream());
+        int secondBodyLength = secondBody.length();
+        assertTrue(secondBodyLength > firstBodyLength);
+    }
+
+    String readAllAndClose(InputStream is) throws Exception {
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        try {
+            int read;
+            while ((read = is.read()) != -1) {
+                out.write(read);
+            }
+        } finally {
+            try {
+                is.close();
+            } catch (Exception e) {
+            }
+        }
+        return out.toString();
+    }
+
+}
